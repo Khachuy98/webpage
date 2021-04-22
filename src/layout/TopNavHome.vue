7c087ddef20cc0b5ac73e414c31f4a9fd31d6b43
Hlee <shyneward123@gmail.com>
2021-04-22 17:34:43 +0700

    update

--- src/layout/TopNavHome.vue
+++ src/layout/TopNavHome.vue
@@ -3,19 +3,17 @@
     <b-navbar class="topnavbar" toggleable="lg" type="dark">
       <b-navbar-brand href="#">SHOES STORE</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-navbar-nav >
-          <router-link class="item-menu" to='/home'>Home</router-link>
-          <router-link class="item-menu" to='/shop'>Shop</router-link>
-          <router-link class="item-menu" to='/about'>About</router-link>
-          <router-link class="item-menu" to='/contact'>Contact</router-link>
-          <router-link class="item-cart" to='/viewcart'>View Cart</router-link>
-       </b-navbar-nav>
-      <b-collapse id="nav-collapse" is-nav> </b-collapse>
-      <div class="wrap-icon">
-        <b-button class="cart-bt">
-          <b-icon icon="cart4" aria-hidden="true" v>Cart</b-icon>
+      <b-navbar-nav>
+        <router-link class="item-menu" to="/home">Home</router-link>
+        <router-link class="item-menu" to="/shop">Shop</router-link>
+        <router-link class="item-menu" to="/about">About</router-link>
+        <router-link class="item-menu" to="/contact">Contact</router-link>
+        <b-button class="cart-bt" to="/viewcart">
+          <b-icon icon="cart4" aria-hidden="true" class="iccart"></b-icon>
         </b-button>
-      </div>
+      </b-navbar-nav>
+      <b-collapse id="nav-collapse" is-nav> </b-collapse>
+      <div class="wrap-icon"></div>
     </b-navbar>
   </div>
 </template>
@@ -25,23 +23,25 @@ export default {};
 </script>
 
 <style scoped>
-    .item-menu{
-      margin-left: 30px;
-      color: black;
-
-    }
-    .item-menu :hover{
-        color: #6c7ae0;
-    }
-    .item-cart{
-      float: right;
-    }
-    .topnavhome{
-        position: fixed;
-        left: 0;
-        top: 0;
-        width: 100%;
-        z-index: 1000;
-        background-color: white;
-    }
+.item-menu {
+  margin-left: 30px;
+  color: black;
+}
+.item-menu :hover {
+  color: #6c7ae0;
+}
+.item-cart {
+  float: right;
+}
+.topnavhome {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  z-index: 1000;
+  background-color: white;
+}
+.cart-bt{
+  transform: translateX(700px);
+}
 </style>
--- src/pages/ViewCart.vue
+++ src/pages/ViewCart.vue
@@ -2,24 +2,128 @@
   <div class="viewcart-1">
     <h2>View Cart</h2>
     <b-container class="content">
-          <b-table
-            sticky-header
-            class="table-ct"
-            striped
-            hover
-            :items="items"
-            :fields="fields"
-            head-variant="dark"
+      <b-table
+        sticky-header
+        class="table-ct"
+        striped
+        hover
+        :items="items"
+        :fields="fields"
+        head-variant="dark"
+      >
+        <template v-slot:cell(actions)="data">
+          <b-button
+            variant="primary"
+            size="sm"
+            class="mr-2"
+            @click="payment(data.id)"
+            >Payment</b-button
           >
-          <template v-slot:cell(actions)="data">
-            <b-button variant="primary" size="sm" class="mr-2" @click="payment(data.id)">Payment</b-button>
-            <b-button variant="danger" size="sm" @click="delete(data.id)">Remove</b-button>
-          </template>
-          </b-table>
-          <b-button variant="primary">Payment</b-button>
+          <b-button variant="danger" size="sm" @click="delete data.id"
+            >Remove</b-button>
+        </template>
+      </b-table>
+      <b-button class="paym" variant="primary" v-b-modal.modal-center>Payment</b-button>
+      <b-link class="comeback" href="#" to="/home">
+      <b-icon icon="arrow-return-left" ></b-icon>
+      Continue Purchase</b-link>
     </b-container>
     <top-nav-home />
     <content-footer-home />
+    <b-modal size="xl" class="pay" id="modal-center" hide-footer>
+      <h3>Shoes Store</h3>
+      <b-row>
+        <b-col lg="4">
+          <b-form-input
+            class="ipmd"
+            v-model="text"
+            placeholder="Enter your phone number"
+          ></b-form-input>
+          <b-form-input
+            class="ipmd"
+            v-model="text"
+            placeholder="Enter your email"
+          ></b-form-input>
+          <b-form-input
+            class="ipmd"
+            v-model="text"
+            placeholder="Enter your name"
+          ></b-form-input>
+          <b-form-input
+            class="ipmd"
+            v-model="text"
+            placeholder="Enter your adress"
+          ></b-form-input>
+        </b-col>
+        <b-col lg="4">
+          <b-form-textarea
+            class="ipmd"
+            id="textarea"
+            v-model="text"
+            placeholder="Enter description"
+            rows="4"
+            max-rows="8"
+          ></b-form-textarea>
+          <b-form-radio
+            class="ipmd"
+            v-model="selected"
+            :aria-describedby="ariaDescribedby"
+            name="some-radios"
+            value="Thanh toán khi nhận hàng"
+            >Payment on delivery</b-form-radio
+          >
+          <b-form-radio
+            class="ipmd"
+            v-model="selected"
+            :aria-describedby="ariaDescribedby"
+            name="some-radios"
+            value="Chuyển khoản"
+            v-b-toggle.collapse-1
+            >Transfer payments
+            <b-collapse id="collapse-1" class="mt-2">
+              <div
+                id="divmethod"
+                class="divmethod"
+                style="overflow: hidden; display: block"
+              >
+                <strong
+                  >Ngân hàng Vietcombank (Ngân hàng ngoại thương Việt
+                  Nam)</strong
+                ><br />
+                - Số tài khoản: 52480201022<br />
+                - Chủ tài khoản: Bùi Khắc Huy<br />
+                - Chi nhánh: Lê Thành Phương - Phường 2
+              </div>
+            </b-collapse>
+          </b-form-radio>
+        </b-col>
+        <b-col lg="4">
+          <h3>Invoice Details</h3>
+          <b-table 
+            sticky-header
+            class="table-bill"
+            striped hover
+            :items="bill"
+            :fields="info"
+          >
+            <template v-slot:cell(actions)="data">
+            <b-icon
+              icon="backspace"
+              aria-hidden="true"
+              size="sm"
+              @click="delete data.id"
+            ></b-icon>
+            </template>
+          </b-table>
+          <div class="total">
+            <div>Total:</div>
+            <b-badge variant="light" class="prtotal">$25.54</b-badge>
+          </div>
+        </b-col>
+      </b-row>
+          <b-button  pill variant="success" class="btsubmit"
+          >Order Complete</b-button>
+    </b-modal>
   </div>
 </template>
 
@@ -30,61 +134,103 @@ export default {
   components: { ContentFooterHome, TopNavHome },
   data() {
     return {
-      fields:[
+      fields: [
         {
-         key: 'name',
-         label: 'Name'
+          key: "name",
+          label: "Name",
         },
         {
-         key: 'email',
-         label: 'Email'
+          key: "email",
+          label: "Email",
         },
         {
-         key: 'age',
-         label: 'Age',
-         sortable: true
+          key: "age",
+          label: "Age",
+          sortable: true,
         },
         {
-         key: 'actions',
-         label: 'Actions',
+          key: "actions",
+          label: "Actions",
         },
-    ],
+      ],
       items: [
         {
           id: 1,
           name: "Huy",
           email: "shyneward123@gmail.com",
-          age: "23"
+          age: "23",
         },
         {
           id: 2,
           name: "Vũ",
           email: "shyneward123@gmail.com",
-          age: "19"
+          age: "19",
         },
-                {
+        {
           id: 2,
           name: "Vũ",
           email: "shyneward123@gmail.com",
-          age: "19"
+          age: "19",
         },
-                {
+        {
           id: 2,
           name: "Vũ",
           email: "shyneward123@gmail.com",
-          age: "19"
+          age: "19",
         },
-                {
+        {
           id: 2,
           name: "Vũ",
           email: "shyneward123@gmail.com",
-          age: "19"
+          age: "19",
         },
-                {
+        {
           id: 2,
           name: "Vũ",
           email: "shyneward123@gmail.com",
-          age: "19"
+          age: "19",
+        },
+      ],
+      info: [
+        {
+          key: "name",
+          label: "",
+        },
+        {
+          key: "size",
+          label: "",
+        },
+        {
+          key: "color",
+          label: "",
+        },
+        {
+          key: "amount",
+          label: "",
+        },
+        {
+          key: "price",
+          label: "",
+        },
+        {
+          key: "actions",
+          label: "",
+        },        
+      ],
+      bill: [
+        {
+          name: "Adidas",
+          size: "Size40",
+          color: "Black",
+          amount: "1",
+          price: "$25.54",
+        },
+                {
+          name: "Converse",
+          size: "Size41",
+          color: "Black",
+          amount: "1",
+          price: "$30.25",
         },
       ],
     };
@@ -95,6 +241,10 @@ export default {
 <style scoped>
 .viewcart-1 {
   margin-top: 80px;
+}
+h3 {
+  text-align: center;
+  font-weight: bolder;
 }
 h2 {
   font-weight: bolder;
@@ -107,5 +257,26 @@ h2 {
 }
 .table-ct {
   color: black;
+}
+.ipmd {
+  margin-top: 20px;
+}
+.btsubmit {
+  display: block !important;
+  margin: 30px auto 0 !important;
+}
+.prtotal{
+  float: right;
+  margin-top: -21px;
+}
+.paym{
+  display: block !important;
+  margin: 30px auto 0 !important;
+}
+.comeback{
+  margin-bottom: 30px !important;
+}
+.table-bill{
+  display: block;
 }
 </style>
